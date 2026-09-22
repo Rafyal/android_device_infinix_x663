34e163a3fb84fcd4f52b3cdee6d776f43a4e7f05
Kyouken <119907284+Rafyal@users.noreply.github.com>
2026-09-22 16:53:35 +0700

    Add new lunch choices for crdroid_X663

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crdroid_X663.mk
 
 COMMON_LUNCH_CHOICES := \
-    crdroid_X663-userdebug
+    crdroid_X663-user \
+    crdroid_X663-userdebug \
+    crdroid_X663-eng
